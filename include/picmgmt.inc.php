871060671cfe7cb43f1c77d00a842ba1c5612850
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2010-10-13 13:32:21 +0000

    Use IPTC headline instead of IPTC title as picture title according to docs (thread ID 61380).

--- include/picmgmt.inc.php
+++ include/picmgmt.inc.php
@@ -66,7 +66,7 @@ function add_picture($aid, $filepath, $filename, $position = 0, $title = '', $ca
             // read IPTC data
             $iptc = get_IPTC($image);
             if (is_array($iptc) && !$title && !$caption && !$keywords) {  //if any of those 3 are filled out we don't want to override them, they may be blank on purpose.
-                $title = (isset($iptc['Title'])) ? $iptc['Title'] : $title;
+                $title = (isset($iptc['Headline'])) ? $iptc['Headline'] : $title;
                 $caption = (isset($iptc['Caption'])) ? $iptc['Caption'] : $caption;
                 $keywords = (isset($iptc['Keywords'])) ? implode($CONFIG['keyword_separator'], $iptc['Keywords']) : $keywords;
             }
