a194809912ecc8529531cec7f4776fc6fb89aae4
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2010-12-15 09:22:55 +0000

    Fixed memberlist issue when database name contains a dash (thread ID 68843).

--- trunk/cpg1.6.x/bridge/udb_base.inc.php
+++ trunk/cpg1.6.x/bridge/udb_base.inc.php
@@ -221,8 +221,8 @@ class core_udb {
         $sql = "SELECT {$f['user_id']} AS user_id, {$f['username']} AS user_name, {$f['email']} AS user_email, {$f['regdate']} AS user_regdate, {$f['lastvisit']} AS user_lastvisit, {$f['active']} AS user_active, "
                . "COUNT(pid) AS pic_count, ROUND(SUM(total_filesize)/1024) AS disk_usage, group_name, group_quota "
                . "FROM {$this->usertable} AS u "
-               . "INNER JOIN {$C['dbname']}.{$C['TABLE_USERGROUPS']} AS g ON u.{$f['usertbl_group_id']} = g.group_id "
-               . "LEFT JOIN {$C['dbname']}.{$C['TABLE_PICTURES']} AS p ON p.owner_id = u.{$f['user_id']} "
+               . "INNER JOIN `{$C['dbname']}`.{$C['TABLE_USERGROUPS']} AS g ON u.{$f['usertbl_group_id']} = g.group_id "
+               . "LEFT JOIN `{$C['dbname']}`.{$C['TABLE_PICTURES']} AS p ON p.owner_id = u.{$f['user_id']} "
                . $options['search']
                . "GROUP BY user_id " . "ORDER BY " . $sort_codes[$options['sort']] . " "
                . "LIMIT {$options['lower_limit']}, {$options['users_per_page']};";
