aecf05a98f2d6f7fd597c90af73eaf929f162f86
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2012-06-07 14:29:25 +0000

    Strip unneeded EXIF data before storing it in the database (thread ID 75011).

--- trunk/cpg1.6.x/include/exif_php.inc.php
+++ trunk/cpg1.6.x/include/exif_php.inc.php
@@ -26,59 +26,41 @@ function exif_parse_file($filename, $pid)
     if (!is_readable($filename)) {
         return false;
     }
-    
+
     $size = cpg_getimagesize($filename);
 
     if ($size[2] != 2) {
         return false; // Not a JPEG file
     }
 
-    //Check if we have the data of the said file in the table
-    $sql = "SELECT exifData FROM {$CONFIG['TABLE_EXIF']} WHERE pid = $pid";
-
-    $result = cpg_db_query($sql);
-
-    if (mysql_num_rows($result) > 0) {
-
-        $row = mysql_fetch_assoc($result);
-        mysql_free_result($result);
-        $exifRawData = unserialize($row['exifData']);
-        
-    } else { // No data in the table - read it from the image file
-    
-        $exifRawData = read_exif_data_raw($filename, 0);
-
-        // Insert it into table for future reference
-        $sql = "INSERT INTO {$CONFIG['TABLE_EXIF']} (pid, exifData) VALUES ($pid, '".addslashes(serialize($exifRawData))."')";
-        $result = cpg_db_query($sql);
-    }
-
-    $exif = array();
-
-    if (is_array($exifRawData['IFD0'])) {
-        $exif = array_merge($exif, $exifRawData['IFD0']);
-    }
-    
-    if (is_array($exifRawData['SubIFD'])) {
-        $exif = array_merge($exif, $exifRawData['SubIFD']);
-    }
-    
-    if (is_array($exifRawData['SubIFD']['MakerNote'])) {
-        $exif = array_merge($exif, $exifRawData['SubIFD']['MakerNote']);
-    }
-
-    if (isset($exifRawData['IFD1OffSet'])) {
-        $exif['IFD1OffSet'] = $exifRawData['IFD1OffSet'];
-    }
-    
-    $exifParsed = array();
-
     //String containing all the available exif tags.
     $exif_info = "AFFocusPosition|Adapter|ColorMode|ColorSpace|ComponentsConfiguration|CompressedBitsPerPixel|Contrast|CustomerRender|DateTimeOriginal|DateTimedigitized|DigitalZoom|DigitalZoomRatio|ExifImageHeight|ExifImageWidth|ExifInteroperabilityOffset|ExifOffset|ExifVersion|ExposureBiasValue|ExposureMode|ExposureProgram|ExposureTime|FNumber|FileSource|Flash|FlashPixVersion|FlashSetting|FocalLength|FocusMode|GainControl|IFD1Offset|ISOSelection|ISOSetting|ISOSpeedRatings|ImageAdjustment|ImageDescription|ImageSharpening|LightSource|Make|ManualFocusDistance|MaxApertureValue|MeteringMode|Model|NoiseReduction|Orientation|Quality|ResolutionUnit|Saturation|SceneCaptureMode|SceneType|Sharpness|Software|WhiteBalance|YCbCrPositioning|xResolution|yResolution";
     $exif_names = explode("|", $exif_info);
-    
+
+    //Check if we have the data of the said file in the table
+    $result = cpg_db_query("SELECT exifData FROM {$CONFIG['TABLE_EXIF']} WHERE pid = $pid");
+    if (mysql_num_rows($result) > 0) {
+        $row = mysql_fetch_assoc($result);
+        mysql_free_result($result);
+        $exif = unserialize($row['exifData']);
+
+        // Convert old EXIF data style to new one
+        if (array_key_exists('Errors', $exif)) {
+            $exif = cpg_exif_strip_data($exif, $exif_names);
+            cpg_db_query("UPDATE {$CONFIG['TABLE_EXIF']} SET exifData = '".addslashes(serialize($exif))."' WHERE pid = '$pid'");
+        }
+    } else { // No data in the table - read it from the image file
+        $exifRawData = read_exif_data_raw($filename, 0);
+
+        $exif = cpg_exif_strip_data($exifRawData, $exif_names);
+
+        // Insert it into table for future reference
+        cpg_db_query("INSERT INTO {$CONFIG['TABLE_EXIF']} (pid, exifData) VALUES ($pid, '".addslashes(serialize($exif))."')");
+    }
+
+    $exifParsed = array();
     $exifCurrentData = array_keys(array_filter(explode("|", $CONFIG['show_which_exif'])));
-    
+
     foreach ($exifCurrentData as $i) { 
         $name = $exif_names[$i];
         if (isset($exif[$name])) {
