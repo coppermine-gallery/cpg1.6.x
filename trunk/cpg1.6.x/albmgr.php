a8c191ded1c1a95061913e4d5f11f04d5e6eef2f
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2012-06-29 10:38:07 +0000

    Fixed preselection of album name when editing albums at the album manager.

--- trunk/cpg1.6.x/albmgr.php
+++ trunk/cpg1.6.x/albmgr.php
@@ -218,7 +218,7 @@ if (count($rowset) > 0) {
         echo <<< EOT
                 <tr id="sort-{$album['aid']}">
                     <td class="dragHandle"></td>
-                    <td class="album_text" width="96%"><span class="albumName">{$title}&nbsp;<a href="upload.php?album={$album['aid']}"><img src="images/icons/upload.png" title="{$lang_albmgr_php['upload_files']}" /></a><span class="editAlbum">{$icon_array['edit']}{$lang_common['edit']}</span></td>
+                    <td class="album_text" width="96%"><span class="albumName">{$title}</span>&nbsp;<a href="upload.php?album={$album['aid']}"><img src="images/icons/upload.png" title="{$lang_albmgr_php['upload_files']}" /></a><span class="editAlbum">{$icon_array['edit']}{$lang_common['edit']}</span></td>
                 </tr>
 EOT;
     }
