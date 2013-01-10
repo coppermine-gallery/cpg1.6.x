ae57332acfa958f8d9dec63e8744fa5c67dc8629
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-01-10 15:35:38 +0000

    Unified header information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 /*************************
-    Coppermine Photo Gallery
-    ************************
-    Copyright (c) 2003-2012 Coppermine Dev Team
-    v1.1 originaly written by Gregory DEMAR
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2012 Coppermine Dev Team
+  v1.0 originaly written by Gregory DEMAR
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 3
-    as published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
 
-    ********************************************
-    Coppermine version: 1.6.01
-    $HeadURL$
-    $Revision$
+  ********************************************
+  Coppermine version: 1.6.01
+  $HeadURL$
+  $Revision$
 **********************************************/
 
 // Object to hold javascript keyCodes for various keys
