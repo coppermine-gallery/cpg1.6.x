1665878819c3a509606ba94ef636cf2b5bf92ffd
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-01-10 16:07:14 +0000

    Updated header information to reflect new year. Preparing release of cpg1.5.22.

--- trunk/cpg1.6.x/js/editpics.js
+++ trunk/cpg1.6.x/js/editpics.js
@@ -1,8 +1,8 @@
 /*************************
   Coppermine Photo Gallery
   ************************
-  Copyright (c) 2003-2012 Coppermine Dev Team
-  v1.0 originaly written by Gregory DEMAR
+  Copyright (c) 2003-2013 Coppermine Dev Team
+  v1.0 originally written by Gregory DEMAR
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 3
--- trunk/cpg1.6.x/plugins/sample/configuration.php
+++ trunk/cpg1.6.x/plugins/sample/configuration.php
@@ -2,7 +2,7 @@
 /*************************
   Coppermine Photo Gallery
   ************************
-  Copyright (c) 2003-2012 Coppermine Dev Team
+  Copyright (c) 2003-2013 Coppermine Dev Team
   v1.0 originally written by Gregory Demar
 
   This program is free software; you can redistribute it and/or modify
