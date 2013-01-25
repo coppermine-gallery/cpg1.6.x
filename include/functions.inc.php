028ef587f02f3c052e3aba490af17eabf48761f0
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-01-25 13:48:38 +0000

    Fixed album drop-down box options for guests (thread ID 75905).

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -5920,6 +5920,8 @@ function album_selection_options($selected = 0)
         $result = cpg_db_query("SELECT aid, title, category FROM {$CONFIG['TABLE_ALBUMS']} ORDER BY pos");
     } elseif (USER_ID) {
         $result = cpg_db_query("SELECT aid, title, category FROM {$CONFIG['TABLE_ALBUMS']} WHERE category = " . (FIRST_USER_CAT + USER_ID) . " OR owner = " . USER_ID . $uploads_yes . " ORDER BY pos");
+    } else {
+        $result = cpg_db_query("SELECT aid, title, category FROM {$CONFIG['TABLE_ALBUMS']} WHERE 0 " . $uploads_yes . " ORDER BY pos");
     }
 
     while ( ($row = mysql_fetch_assoc($result)) ) {
@@ -5936,8 +5938,10 @@ function album_selection_options($selected = 0)
     // Load all categories
     if (GALLERY_ADMIN_MODE) {
         $result = cpg_db_query("SELECT cid, rgt, name FROM {$CONFIG['TABLE_CATEGORIES']} ORDER BY lft");
-    } else {
+    } elseif (USER_ID) {
         $result = cpg_db_query("SELECT DISTINCT c.cid, c.rgt, c.name FROM {$CONFIG['TABLE_ALBUMS']} AS a RIGHT JOIN {$CONFIG['TABLE_CATEGORIES']} AS c ON a.category = c.cid WHERE c.cid = " . USER_GAL_CAT . " OR a.owner = ". USER_ID . $uploads_yes . " ORDER BY lft");
+    } else {
+        $result = cpg_db_query("SELECT DISTINCT c.cid, c.rgt, c.name FROM {$CONFIG['TABLE_ALBUMS']} AS a RIGHT JOIN {$CONFIG['TABLE_CATEGORIES']} AS c ON a.category = c.cid WHERE 0 " . $uploads_yes . " ORDER BY lft");
     }
 
     $cats = array();
@@ -5991,6 +5995,8 @@ function album_selection_options($selected = 0)
             $options .= '</optgroup>';
         }
     }
+    mysql_free_result($result);
+
     return $options;
 }
 // end function album_selection_options
