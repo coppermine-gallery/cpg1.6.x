82b8cc1f4fa0347c6355f925dcf069af24c95717
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2013-08-26 07:52:27 +0000

    Fixed error message when removing favorite pictures (thread ID 76549).

--- addfav.php
+++ addfav.php
@@ -44,6 +44,43 @@ if (!in_array($pid, $FAVPICS)) {
     unset($FAVPICS[$key]);
 }
 
+// Adjust redirect for "My Favorites" meta album
+if (!empty($CPG_REFERER) && strpos($CPG_REFERER, 'album=favpics')) {
+    if (empty($FAVPICS)) {
+        // No favorites, redirect to empty thumbnail page
+        $ref = $CONFIG['site_url'] . "thumbnails.php?album=favpics";
+    } elseif (!in_array($pid, $FAVPICS)) {
+        // It may be possible that there are PIDs in the $FAVPICS array which has been removed from the gallery or the user has (temporary) no access to.
+        // That's why we need to query the database to get the current accessible PIDs and redirect the user accordingly.
+        // We don't update the $FAVPICS array in case the user gets access to any pictures at a later date
+
+        $query = "SELECT pid FROM {$CONFIG['TABLE_PICTURES']} AS p
+            INNER JOIN {$CONFIG['TABLE_ALBUMS']} AS r ON r.aid = p.aid
+            $RESTRICTEDWHERE
+            AND approved = 'YES'
+            AND pid IN (".implode(', ', $FAVPICS).", $pid)
+            ORDER BY pid DESC";
+        $result = cpg_db_query($query);
+
+        if (mysql_num_rows($result) == 1) {
+            // No favorites (as we added the already removed $pid to the query), redirect to empty thumbnail page
+            $ref = $CONFIG['site_url'] . "thumbnails.php?album=favpics";
+        } else {
+            while ($row = mysql_fetch_assoc($result)) {
+                if ($row['pid'] == $pid && $new_pid) {
+                    break;
+                }
+                $new_pid = $row['pid'];
+                if ($new_pid < $pid) {
+                    break;
+                }
+            }
+            $ref = str_replace("pid={$pid}", "pid={$new_pid}", $ref);
+        }
+        mysql_free_result($result);
+    }
+}
+
 $data = base64_encode(serialize($FAVPICS));
 if (CPG_COOKIES_ALLOWED) {
     setcookie($CONFIG['cookie_name'].'_fav', $data, time() + (CPG_DAY*30), $CONFIG['cookie_path']);
