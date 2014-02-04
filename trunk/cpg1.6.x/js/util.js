a511fbd358c4435ad47c6eefbf56dcedb22f6e0d
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2014-02-04 15:31:26 +0000

    Replaced some jQuery code with plain JavaScript code to make admin tools compatible with later jQuery versions, in case users want to upgrade (thread ID 76858).

--- trunk/cpg1.6.x/js/util.js
+++ trunk/cpg1.6.x/js/util.js
@@ -21,7 +21,7 @@ function init_utils(){
     jQuery.each($("input[type='radio'][name='action']"), function(){
         $(this).change(function(){
             jQuery.each($("input[type='radio'][name='action']"), function(){
-                $('#' + $(this).attr('id') + '_wrapper').css('display', ($(this).attr('checked')) ? 'block' : 'none');                    
+                $('#' + this.getAttribute('id') + '_wrapper').css('display', (this.checked) ? 'block' : 'none');
             });
         });               
     });
