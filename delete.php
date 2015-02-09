f702c5a714545103cb57a9edd1dcdf438fca08a0
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2015-02-09 21:10:56 +0000

    Fixed icon when deleting picture from an album (thread ID 78036).

--- delete.php
+++ delete.php
@@ -161,9 +161,6 @@ function delete_picture($pid, $tablecellstyle = 'tableb')
     $query = "DELETE FROM {$CONFIG['TABLE_PICTURES']} WHERE pid='$pid' LIMIT 1";
     cpg_db_query($query);
 
-    $query = "UPDATE {$CONFIG['TABLE_ALBUMS']} SET thumb = '0' WHERE thumb = '$pid'";
-    cpg_db_query($query);
-
     echo "<td class=\"".$tablecellstyle."\" align=\"center\">";
 
     if (mysql_affected_rows($CONFIG['LINK_ID']) > 0) {
@@ -171,6 +168,9 @@ function delete_picture($pid, $tablecellstyle = 'tableb')
     } else {
         echo $red;
     }
+
+    $query = "UPDATE {$CONFIG['TABLE_ALBUMS']} SET thumb = '0' WHERE thumb = '$pid'";
+    cpg_db_query($query);
 
     echo '</td>';
     echo '</tr>' . $LINEBREAK;
