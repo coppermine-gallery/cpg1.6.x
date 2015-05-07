46862998903ec07503531fa6aaa0a55de9c6d6be
eenemeenemuu <eenemeenemuu@f8ca792f-0228-4f31-972d-500d6385409f>
2015-05-07 10:09:26 +0000

    Fixed various security vulnerabilities (thread ID 78158).

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -6633,4 +6633,38 @@ function only_empty_albums_button() {
     return $only_empty_albums;
 }
 
+
+/**
+ * cpg_normalize_path
+ *
+ * Method to normalize a virtual path that could handle .. references that go beyond the initial folder reference
+ * Taken from http://php.net/manual/en/function.realpath.php#112367
+ */
+function cpg_normalize_path($path) {
+    $parts = array(); // Array to build a new path from the good parts
+    $path = str_replace('\\', '/', $path); // Replace backslashes with forwardslashes
+    $path = preg_replace('/\/+/', '/', $path); // Combine multiple slashes into a single slash
+    $segments = explode('/', $path); // Collect path segments
+    $test = ''; // Initialize testing variable
+    foreach ($segments as $segment) {
+        if ($segment != '.') {
+            $test = array_pop($parts);
+            if (is_null($test)) {
+                $parts[] = $segment;
+            } elseif ($segment == '..') {
+                if ($test == '..') {
+                    $parts[] = $test;
+                }
+                if ($test == '..' || $test == '') {
+                    $parts[] = $segment;
+                }
+            } else {
+                $parts[] = $test;
+                $parts[] = $segment;
+            }
+        }
+    }
+    return implode(DIRECTORY_SEPARATOR, $parts);
+}
+
 ?>
