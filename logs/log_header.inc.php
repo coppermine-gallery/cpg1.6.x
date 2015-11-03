dfadb36ee317399ddd984d4721e8ab0ba8c12c14
ron4mac <ron@rjconline.net>
2015-11-03 07:59:34 -0500

    Create log_header.inc.php

---
+++ logs/log_header.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2014 Coppermine Dev Team
+  v1.0 originally written by Gregory Demar
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.6.01
+  $HeadURL$
+  $Revision$
+**********************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+?>
