00d0ff5218fb8c2b385713802ddfb8e326d6fa8e
ron4mac <ron@rjconline.net>
2016-01-19 08:29:42 -0600

    remove old swfupload js

--- uniload.php
+++ uniload.php
@@ -1,4 +1,20 @@
 <?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2016 Coppermine Dev Team
+  v1.0 originally written by Gregory Demar
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.6.01
+  $HeadURL$
+  $Revision$
+**********************************************/
+
 // Confirm we are in Coppermine and set the language blocks.
 define('IN_COPPERMINE', true);
 define('UPLOAD_PHP', true);
