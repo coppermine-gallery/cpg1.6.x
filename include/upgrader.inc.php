d4c8147b93a5c9c9753cb47965bce719ebdd0858
ron4mac <ron@rjconline.net>
2016-05-26 09:09:30 -0500

    correct possible failure to get update when curl must be used

--- include/upgrader.inc.php
+++ include/upgrader.inc.php
@@ -139,6 +139,7 @@ class CPG_Updater
 		//return the transfer as a string
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		// $output contains the output string
 		$output = curl_exec($ch);
 		// close curl resource to free up system resources
