ae4b64d4fe80599c1f4008f61484bd3e4a159e29
ron4mac <ron@rjconline.net>
2017-04-21 10:31:55 -0400

    MODIFY sql update

--- sql/update.sql
+++ sql/update.sql
@@ -30,14 +30,15 @@ INSERT INTO CPG_config VALUES ('custom_sortorder_thumbs', '1');
 INSERT INTO CPG_config VALUES ('link_last_upload', '0');
 INSERT INTO CPG_config VALUES ('editpics_ignore_newer_than', '0');
 
+ALTER TABLE CPG_users MODIFY user_password varchar(255) NOT NULL default '';
 ALTER TABLE CPG_users ADD user_password_salt varchar(255) NOT NULL default '';
 ALTER TABLE CPG_users ADD user_password_hash_algorithm varchar(25) NOT NULL default '';
 ALTER TABLE CPG_users ADD user_password_iterations varchar(25) NOT NULL default '';
 
-ALTER TABLE CPG_comments CHANGE `msg_date` `msg_date` datetime NOT NULL default '1000-01-01 00:00:00';
-ALTER TABLE CPG_pictures CHANGE `mtime` `mtime` datetime NOT NULL default '1000-01-01 00:00:00';
-ALTER TABLE CPG_users CHANGE `user_lastvisit` `user_lastvisit` datetime NOT NULL default '1000-01-01 00:00:00';
-ALTER TABLE CPG_users CHANGE `user_regdate` `user_regdate` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_comments MODIFY `msg_date` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_pictures MODIFY `mtime` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_users MODIFY `user_lastvisit` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_users MODIFY `user_regdate` datetime NOT NULL default '1000-01-01 00:00:00';
 
 INSERT INTO CPG_config VALUES ('batch_add_hide_existing_files', '0');
 INSERT INTO CPG_config VALUES ('only_empty_albums', '0');
