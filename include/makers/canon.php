9016d27827ceebd9051f35e643ae873eda19bbac
ron4mac <ron@rjconline.net>
2018-10-17 12:20:24 -0500

    correct file reference

--- include/makers/canon.php
+++ include/makers/canon.php
@@ -7,7 +7,7 @@
  * @copyright  Copyright (c) 2003-2018 Coppermine Dev Team
  * @license    GNU General Public License version 3 or later; see LICENSE
  *
- * install.php
+ * include/makers/canon.php
  * @since  1.6.05
  */
 //================================================================================================
