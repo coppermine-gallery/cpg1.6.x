01dc7e346413582167afe6fe7ba84505d3b309e1
ron4mac <ron@rjconline.net>
2020-02-26 17:17:18 -0600

    release version 1.6.08

--- include/init.inc.php
+++ include/init.inc.php
@@ -11,7 +11,7 @@
  * @since  1.6.08
  */
 
-define('COPPERMINE_VERSION', '1.6.07');
+define('COPPERMINE_VERSION', '1.6.08');
 define('COPPERMINE_VERSION_STATUS', 'stable');
 // Define path to jQuery for this version of Coppermine
 define('CPG_JQUERY_VERSION', 'js/jquery-1.7.2.js');
