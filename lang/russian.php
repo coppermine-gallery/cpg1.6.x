4a09e5652cfe88d96a7b5efcf3daac781db1de1b
SiavaRu <siava@siava.ru>
2021-02-10 12:37:56 +0300

    Update for 1.6.10 version CPG

--- lang/russian.php
+++ lang/russian.php
@@ -4,11 +4,11 @@
  *
  * v1.0 originally written by Gregory Demar
  *
- * @copyright  Copyright (c) 2003-2018 Coppermine Dev Team
+ * @copyright  Copyright (c) 2003-2021 Coppermine Dev Team
  * @license    GNU General Public License version 3 or later; see LICENSE
  *
- * lang/english.php
- * @since  1.6.08
+ * lang/russian.php
+ * @since  1.6.10
  */
 
 if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
