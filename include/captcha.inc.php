887058192ba09a7d0a5a492fc09fa8f5f9579125
ron4mac <ron@rjconline.net>
2025-07-20 11:51:55 +0000

    correct deprecation in captcha

--- include/captcha.inc.php
+++ include/captcha.inc.php
@@ -1,18 +1,15 @@
 <?php
-/*************************
-  Coppermine Photo Gallery
-  ************************
-  Copyright (c) 2003-2016 Coppermine Dev Team
-  v1.0 originally written by Gregory Demar
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 3
-  as published by the Free Software Foundation.
-
-  ********************************************
-  Coppermine version: 1.6.03
-  $HeadURL$
-**********************************************/
+/**
+ * Coppermine Photo Gallery
+ *
+ * v1.0 originally written by Gregory Demar
+ *
+ * @copyright  Copyright (c) 2003-2025 Coppermine Dev Team
+ * @license    GNU General Public License version 3 or later; see LICENSE
+ *
+ * include/captcha.inc.php
+ * @since  1.6.27
+ */
 
  /***************************************************************/
  /* PhpCaptcha                                                  */
@@ -137,9 +134,9 @@ if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
             $aCharDetails = imageftbbox($iFontSize, $iAngle, $sCurrentFont, $this->sCode[$i]);
 
             // calculate character starting coordinates
-            $iX = $this->iSpacing / 4 + $i * $this->iSpacing;
+            $iX = intdiv($this->iSpacing, 4) + $i * $this->iSpacing;
             $iCharHeight = $aCharDetails[2] - $aCharDetails[5];
-            $iY = $this->iHeight / 2 + $iCharHeight / 4;
+            $iY = intdiv($this->iHeight, 2) + intdiv($iCharHeight, 4);
 
             // write text to image
             imagefttext($this->oImage, $iFontSize, $iAngle, $iX, $iY, $iTextColour, $sCurrentFont, $this->sCode[$i]);
